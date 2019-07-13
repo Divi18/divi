f4e4d6de62f6947374cea09d6c3c4713c5cfaf30
Divi18 <52823069+Divi18@users.noreply.github.com>
2019-07-13 15:34:34 +0530

    Update hunt1.py

--- hunt1.py
+++ hunt1.py
@@ -7,9 +7,9 @@ if(n>0 and n<10000):
   for i in l2:
     if l.count(i)>1:
       l1.append(i)
-   if(l1==[]):
+  if(l1==[]):
     print("unique")
-   else:
+  else:
     l1.sort()
     s=""
     for i in l1:
