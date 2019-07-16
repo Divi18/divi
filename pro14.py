ea52327192bfd574137af7756f52e2fac3beeb63
Divi18 <52823069+Divi18@users.noreply.github.com>
2019-07-16 11:43:18 +0530

    Update pro14.py

--- pro14.py
+++ pro14.py
@@ -2,7 +2,7 @@ p1,q1=map(int,input().split())
 l1=list(map(int,input().split()))
 for i in range(q1):
   r1,s1=map(int,input().split())
-  t1=l[r1-1:s1]
+  t1=l1[r1-1:s1]
   u1=t1[0]
   for i in range(1,len(t1)):
     u1=u1^t1[i]
