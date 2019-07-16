914740f89c42b2d3d98269d5bce07586c68c1011
Divi18 <52823069+Divi18@users.noreply.github.com>
2019-07-16 10:56:41 +0530

    Update pro8.py

--- pro8.py
+++ pro8.py
@@ -3,9 +3,9 @@ p1,q1=map(int,input().split())
 rr=[]
 ss=list(map(int,input().split())
 for i in range(0,q1):
-  u1,v1=map(int,input().split())
-  rr.append([u1,v1])
+        u1,v1=map(int,input().split())
+        rr.append([u1,v1])
 for i in rr:
-  xx=i[0]-1
-  yy=i[1]-1
-  print(math.gcd(ss[xx],ss[yy]))
+        xx=i[0]-1
+        yy=i[1]-1
+        print(math.gcd(ss[xx],ss[yy]))
