a3dc8effa510c602d9fe96e8929f30e94bb7fa8b
Divi18 <52823069+Divi18@users.noreply.github.com>
2019-07-16 10:26:32 +0530

    Create pro4.py

---
+++ pro4.py
@@ -0,0 +1,11 @@
+a1,b1=map(str,input().split())
+y1=0
+if(len(a1)>len(b1)):
+  a1,b1=b1,a1
+z1=0
+while(z1<len(a1)):
+  y1+=(ord(b1[z1])-ord(a1[z1]))
+  z1+=1
+for z1 in range(z1,len(b1)):
+  y1+=ord(b1[z1)-ord('a1')+1
+print(y1)
