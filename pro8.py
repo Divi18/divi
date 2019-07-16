91944c6106c6beb5ee2428343fe8b7792ef674f9
Divi18 <52823069+Divi18@users.noreply.github.com>
2019-07-16 11:01:16 +0530

    Update pro8.py

--- pro8.py
+++ pro8.py
@@ -1,10 +1,10 @@
 import math
 p1,q1=map(int,input().split())
 rr=[]
-ss=list(map(int,input().split())
+ss=list(map(int,input().split()))
 for i in range(0,q1):
-        u1,v1=map(int,input().split())
-        rr.append([u1,v1])
+        uu,vv =map(int,input().split())
+        rr.append([uu,vv])
 for i in rr:
         xx=i[0]-1
         yy=i[1]-1
