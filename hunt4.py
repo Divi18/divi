cc5a0bffaef28439e342deb13b8abd87ef27b337
Divi18 <52823069+Divi18@users.noreply.github.com>
2019-08-07 10:46:55 -0700

    Update hunt4.py

--- hunt4.py
+++ hunt4.py
@@ -1,7 +1,7 @@
 n=int(input())
 if(n>=1 and n<=100000):
   l=list(map(int,input().split(" ")))
-  for j in l:
-    if(l.count(j)==1):
-      print(j)
+  for i in l:
+    if(l.count(i)==1):
+      print(i)
       break
