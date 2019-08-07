ceb92599a467c69c25c6cc8b3d889c4d041aa73c
Divi18 <52823069+Divi18@users.noreply.github.com>
2019-08-07 07:04:50 -0700

    Update hunt.py

--- hunt.py
+++ hunt.py
@@ -8,9 +8,9 @@ if(N>=1 and N<=100000):
         continue
       else:
         l1.append(i)
-l1.sort()
-s=""
-for i in range(0,len(l1)-1):
-  s+=str(l1[i])+" "
-s+=str(l1[len(l1)-1])
-print(s[0:len(s)])
+  l1.sort()
+  s=""
+  for i in range(0,len(l1)-1):
+    s+=str(l1[i])+" "
+  s+=str(l1[len(l1)-1])
+  print(s[0:len(s)])
