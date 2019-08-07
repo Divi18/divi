5cd940425c10af9bf46a49a0c1bde6a765f4db6d
Divi18 <52823069+Divi18@users.noreply.github.com>
2019-08-07 07:00:47 -0700

    Create hunt3.py

---
+++ hunt3.py
@@ -0,0 +1,12 @@
+N=int(input())
+if(N>=1 and N<=100000):
+  l=list(map(int,input().split(" ")))
+  flag=0
+  for i in l:
+    if(l.count(i)>1):
+      l1=i
+      flag=1
+  if(flag==1):
+    print(l1)
+  else:
+    print("unique")
