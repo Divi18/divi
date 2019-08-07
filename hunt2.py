af904dd8b6f2788d4fc42a6b14657baa722f7eee
Divi18 <52823069+Divi18@users.noreply.github.com>
2019-08-07 06:48:58 -0700

    Update hunt2.py

--- hunt2.py
+++ hunt2.py
@@ -1,8 +1,11 @@
 N=int(input())
 if(N>=1 and N<=100000):
   l=list(map(int,input().split(" ")))
-  l.sort(reverse=True)
-  s=""
-  for j in l:
-    s+=str(j)
-  print(s)
+  if(l.count(0)==N)
+    print(0)
+  else:
+    l.sort(reverse=True)
+    s=""
+    for j in l:
+      s+=str(j)
+    print(s)
